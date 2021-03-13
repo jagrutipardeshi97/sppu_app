524599824b0380ecf9aa34dd56b600ca1ab88953
jagruti pardeshi <jagrutipardeshi97@gmail.com>
2021-03-13 20:21:43 +0530

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Welcome to sppu',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
