4cd93a2502e7329d4ba789ac8c8011ac6e13bf17
vikasvarnekar3 <vikasvarnekar3@gmail.com>
2021-03-23 09:58:35 +0530

    add Admin Layout Pages

---
+++ lib/Screens/Admin_Register.dart
@@ -0,0 +1,237 @@
+import"package:flutter/material.dart";
+import 'package:flutter/services.dart';
+
+class Admin extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _Admin();
+  }
+}
+
+class _Admin extends State<Admin> {
+
+  @override
+  Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+    // TODO: implement build
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Container(
+                height: _screenSize.height * 2.0,
+                child: GestureDetector(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        height: double.infinity,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color(0x665ac18e),
+                                Color(0x995ac18e),
+                                Color(0xcc5ac18e),
+                                Color(0xff5ac18e),
+                              ]
+                          ),
+                        ),
+
+
+                        child: SingleChildScrollView(
+                          // physics: AlwaysScrollableScrollPhysics(),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 25,
+                            vertical: 120,
+                          ),
+                          child: Column(
+
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(22.0),
+                                child: Text(
+                                  " Admin Registration Page",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              buildName(),
+                              SizedBox(
+                                  height: 10
+                              ),
+
+                              SizedBox(
+                                height: 10,
+                              ),
+                              buildEmail(),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Loginbutton(),
+                              forgotbutton(),
+
+
+                            ],
+                          ),
+
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+            )
+
+
+        ));
+  }
+
+
+  Widget buildName() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+
+        SizedBox(height: 10,),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                )
+              ]
+          ),
+          height: 60,
+          child: TextField(
+            //   keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.black87,
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: 12,),
+              hintText: "Name",
+              hintStyle: TextStyle(
+                color: Colors.black,
+
+              ),
+            ),
+          ),
+
+        ),
+
+      ],
+
+    );
+  }
+
+
+  Widget buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10,),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                )
+              ]
+          ),
+          height: 60,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.black87,
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: 12,),
+              hintText: "Mail Address Register To University",
+              hintStyle: TextStyle(
+                color: Colors.black,
+
+              ),
+            ),
+          ),
+
+        ),
+
+      ],
+
+    );
+  }
+
+  Widget Loginbutton() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25),
+      width: double.infinity,
+
+      // ignore: deprecated_member_use
+      child: RaisedButton(
+        elevation: 5,
+        onPressed: () => print("Submit"),
+        padding: EdgeInsets.all(15),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15)
+        ),
+        color: Colors.white,
+        child: Text(
+          "Submit",
+          style: TextStyle(
+            color: Color(0xff5ac18e),
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+
+          ),
+
+        ),
+      ),
+
+    );
+  }
+
+  Widget forgotbutton() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: FlatButton(
+        onPressed: () => print("forgot password pressed"),
+        padding: EdgeInsets.only(right: 0),
+
+        child: Text(
+          "Already Register? Login Here",
+
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+
+        ),
+
+      ),
+
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,22 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+import "package:flutter/material.dart";
+import './Screens/Password_Reset_Admin.dart';
+import './Screens/Admin_Register.dart';
+import './Screens/Verify_Admin.dart';
+void main()=> runApp(myapp());
+class myapp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to sppu',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+
+    //home: Admin(),
+    home:PasswordReset(),
+    //home:VerifyAdmin(),
+  );
+
+
+
+
+
+
 }
+
