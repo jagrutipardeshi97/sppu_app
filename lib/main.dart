e1fa3e0291de4d17a5d9bc2124373b55095fc195
USer <priyankasmhetre06@gmail.com>
2021-03-24 22:05:11 +0530

    select_role and splash_screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:sppu_app/Screens/select_role.dart';
+import 'package:sppu_app/Screens/splash_screen.dart';
 import './Screens/Password_Reset_Admin.dart';
 import './Screens/Admin_Register.dart';
 import './Screens/Verify_Admin.dart';
@@ -9,7 +11,9 @@ class myapp extends StatelessWidget{
     debugShowCheckedModeBanner: false,
 
     //home: Admin(),
-    home:PasswordReset(),
+  //home:PasswordReset(),
+  home: splash_screen(),
+    //home: select_role(),
     //home:VerifyAdmin(),
   );
 
